63283646fb96b76f9ba224e650155672726c6e55
Amund Fremming <AmundFremming@Finns-MBP-2.home>
2023-08-04 11:03:48 +0200

    Fiksed the structure in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,26 @@ const App = () => {
         // sets all other states to false so only game is live
     };
 
+    const allPlayersReady = () => {
+        // onSnapshot to the game that is created
+        // Checks if all players are ready
+        // If all players ready, set gameStarted to true
+    };
+
+    const createGame = () => {
+        // Creates a new game in the database
+        // With all properties needed
+    };
+
+    const playerJoinGame = () => {
+        // Takes gameid and username
+        // Joins the given game
+    };
+
+    const getPlayersTurn = () => {
+
+    };
+
     const resetAllGameStates = () => {
         setJoinGame(false);
         setHostGame(false);
@@ -33,73 +53,53 @@ const App = () => {
     if(!joinGame && !hostGame && !hostLobby && !joinLobby && !gameStarted) {
         return(
             <Home
-            joinGame={joinGame}
-            hostGame={hostGame}
-            setJoinGame={setJoinGame}
-            setHostGame={setHostGame}
-        />  
+                joinGame={joinGame}
+                hostGame={hostGame}
+                setJoinGame={setJoinGame}
+                setHostGame={setHostGame}
+            />  
         );
+    } else if (joinGame) {
+        return(
+            <JoinGame
+                resetAllGameStates={resetAllGameStates}
+                gameid={gameid}
+                setGameid={setGameId}
+                username={username}
+                setUsername={setUsername}
+                setJoinLobby={setJoinLobby}
+            />
+        )
+    } else if(hostGame) {
+        return(
+            <HostGame
+                resetAllGameStates={resetAllGameStates}
+                gameid={gameid}
+                setGameid={setGameId}
+                username={username}
+                setUsername={setUsername}
+                setHostLobby={setHostLobby}
+            />
+        )
+    } else if (hostLobby) {
+        return(
+            <HostLobby
+                resetAllGameStates={resetAllGameStates}
+                gameid={gameid}
+                username={username}
+                setGameStarted={setGameStarted}
+            />
+        )
+    } else if(joinLobby) {
+        return(
+            <JoinLobby
+                resetAllGameStates={resetAllGameStates}
+                gameid={gameid}
+                username={username}
+                setGameStarted={setGameStarted}
+            />
+        )
     }
-
-    return(
-        <>
-            {
-                !joinGame && !hostGame && !hostLobby && !joinLobby && !gameStarted ?
-                <Home
-                    joinGame={joinGame}
-                    hostGame={hostGame}
-                    setJoinGame={setJoinGame}
-                    setHostGame={setHostGame}
-                /> : <></>
-            }
-            {
-                joinGame ?
-                <JoinGame
-                    resetAllGameStates={resetAllGameStates}
-                    gameid={gameid}
-                    setGameid={setGameId}
-                    username={username}
-                    setUsername={setUsername}
-                    setJoinLobby={setJoinLobby}
-                /> : <></>
-            }
-            { 
-                hostGame ?
-                <HostGame
-                    resetAllGameStates={resetAllGameStates}
-                    gameid={gameid}
-                    setGameid={setGameId}
-                    username={username}
-                    setUsername={setUsername}
-                    setHostLobby={setHostLobby}
-                /> : <></>
-            }
-            {
-                hostLobby ?
-                <HostLobby
-                    resetAllGameStates={resetAllGameStates}
-                    gameid={gameid}
-                    username={username}
-                    setGameStarted={setGameStarted}
-                /> : <></>
-            }
-            {
-                joinLobby ?
-                <JoinLobby
-                    resetAllGameStates={resetAllGameStates}
-                    gameid={gameid}
-                    username={username}
-                    setGameStarted={setGameStarted}
-                /> : <></>
-            }
-            {
-                gameStarted ?
-                <Game 
-
-                /> : <></>
-            }
-        </>
-    );
 };
 
 export default App;
