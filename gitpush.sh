0c4e33b0039e45404078c541d0c0eb6e72d42f87
amund-fremming <amund.fremming@gmail.com>
2023-08-13 09:29:27 +0200

    added some shortcuts to my bash file

--- gitpush.sh
+++ gitpush.sh
@@ -5,9 +5,35 @@ commitmsg=$1
 if [ -z "$commitmsg" ]; then
 	echo "Provide a commit message."
 	exit 1
+elif [ "$commitmsg" == "ur" ]; then
+	git pull
+	git add .
+	git commit -m "Updated README"
+	git push
+elif [ "$commitmsg" == "cc" ]; then
+	git pull
+	git add .
+	git commit -m "Cleaned up some code"
+	git push
+elif [ "$commitmsg" == "bug" ]; then
+	git pull
+	git add .
+	git commit -m "Fixed some bugs"
+	git push
+elif [ "$commitmsg" == "doc" ]; then
+	git pull
+	git add .
+	git commit -m "Added and cleaned up some documentation"
+	git push
+elif [ "$commitmsg" == "help" ]; then
+	echo "ALL COMMANDS"
+	echo "  ur   Update README"		
+	echo "  cc   Clean up some code"
+	echo "  bug  Fixed some bugs"
+	echo "  doc  Added some documentation"
+else
+	git pull
+	git add .
+	git commit -m "$commitmsg"
+	git push
 fi
-
-git pull
-git add .
-git commit -m "$commitmsg"
-git push
