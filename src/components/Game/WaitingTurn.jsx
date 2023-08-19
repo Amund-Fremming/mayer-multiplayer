91069e0297846d86c7d6b8a03419cf036e9fc2b2
amund-fremming <amund.fremming@gmail.com>
2023-08-19 20:44:46 +0200

    Removed wrong

---
+++ src/components/Game/WaitingTurn.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function WaitingTurn() {
+
+  const pokePlayer = (username) => {
+
+  };
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        Waiting for your turn...
+      </div>
+    </>
+  )
+};
+
+export default WaitingTurn
