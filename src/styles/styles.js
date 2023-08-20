6550c8df21d57124a2e2abc95bb943d502fe1a9a
amund-fremming <amund.fremming@gmail.com>
2023-08-20 19:55:02 +0200

    Updated Styles.js

---
+++ src/styles/styles.js
@@ -0,0 +1,29 @@
+/* TAILWIND STYLES FOR STANDARD ELEMENTS */
+export const pagePaddingAndMargins = "";
+
+const styles = {
+    // Headers
+    mainHeader: "",
+    sectionHeader: "",
+    sectionSubHeader: "",
+
+    // Text
+    sectionText: "",
+    sectionSubText: "",
+
+    // Buttons
+    button3d: "",
+    button2d: "",
+
+    // Input
+    userInput: "",
+
+    // Error messages
+    errorMessage: "",
+
+    // Padding
+    paddingX: "",
+    paddingY: "",
+};
+
+export { styles };
