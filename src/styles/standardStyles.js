4e42e6a25bf6de920e975c2a7db706ac598b312c
amund-fremming <amund.fremming@gmail.com>
2023-08-20 10:27:19 +0200

    Updated standared Styles

--- src/styles/standardStyles.js
+++ src/styles/standardStyles.js
@@ -1,4 +1,4 @@
-
+/* TAILWIND STYLES FOR STANDARD ELEMENTS */
 export const sectionHeader = "";
 
 export const mainHeader = "";
@@ -6,4 +6,13 @@ export const subHeader = "";
 
 export const mainParagraph = "";
 export const subParagraph = ""; 
+
+
+export const button3d = "";
+export const button2d = "";
+
+export const userInput = "";
+
+
+export const pagePaddingAndMargins = "";
 
