5ed7d5fab3e277e4a5ee0eeecdb549dbef6e711f
amund-fremming <amund.fremming@gmail.com>
2023-08-22 10:14:03 +0200

    prod

--- src/views/JoinLobby.jsx
+++ src/views/JoinLobby.jsx
@@ -49,7 +49,7 @@ const JoinLobby = ({ gameid, username, setView, resetGameState, documentRef, sav
             </div>
 
             {/* Info section */}
-            <div className="text-2xl font-oswald text-gray-400 w-full flex flex-col justify-end items-center absolute bottom-7">
+            <div className="text-2xl font-oswald text-gray-400 w-full flex flex-col justify-end items-center absolute bottom-10">
                 <div className="flex w-full justify-center">
                     <p>Waiting for host &nbsp;</p>
                     <Typewriter 
